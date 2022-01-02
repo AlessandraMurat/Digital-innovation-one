39af3fede247d8d9edb72470dbefe742b140c439
Alessandra Murat <alessandra.murat@outlook.com>
2022-01-02 15:54:23 -0300

    Incrementando automatização para indedntificar alterações

--- Node/Arquitetura Rest/src/index.ts
+++ Node/Arquitetura Rest/src/index.ts
@@ -1,2 +1,11 @@
+import express, {Request, Response, NextFunction} from "express";
 
-console.log("Executei")
+const app = express();
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({ foo: 'success'});
+});
+
+app.listen(3000, () => {
+    console.log("Servidor executando na porta 3000")
+})
